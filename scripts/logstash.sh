6b25e58c3754e1965319b9f980878a72231e9edb
Raphaël SCHITZ <raphael@schitz.net>
2020-11-19 10:22:25 +0100

    Create logstash.sh

---
+++ scripts/logstash.sh
@@ -0,0 +1,8 @@
+#
+apt-get -y install logstash
+#
+/bin/systemctl daemon-reload
+systemctl enable logstash.service
+#
+sed -i -e "s/User=logstash/User=root/g" /etc/systemd/system/logstash.service
+sed -i -e "s/Group=logstash/Group=root/g" /etc/systemd/system/logstash.service
