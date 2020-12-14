207b6c3885b59354fc7a6c413cb94340b14e2770
Raphaël SCHITZ <raphael@schitz.net>
2020-12-14 09:47:06 +0100

    Update kibana.sh

--- scripts/kibana.sh
+++ scripts/kibana.sh
@@ -6,6 +6,23 @@ apt-get -y install kibana
 /bin/systemctl daemon-reload
 /bin/systemctl enable kibana.service
 #
+sed -i -e 's/#server\.host: "localhost"/server\.host: "0\.0\.0\.0"/g' /etc/kibana/kibana.yml
+#
+/bin/systemctl start kibana.service
+# Create index pattern
+curl -XPOST "http://localhost:5601/api/saved_objects/index-pattern/logstash-*" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d'
+{
+  "attributes" : {
+    "title" : "logstash-*",
+    "timeFieldName" : "@timestamp"
+  }
+}'
+# Make it the default index
+curl -XPOST "http://localhost:5601/api/kibana/settings/defaultIndex" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d'
+{
+  "value" : "logstash-*"
+}'
+#
 # POST <kibana-host>/api/kibana/settings/theme:darkMode
 # {
 # 	"value": true
