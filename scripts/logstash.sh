a4bcacf4498cee947336e02be539db73cdf713ab
Raphaël SCHITZ <raphael@schitz.net>
2020-12-14 09:47:05 +0100

    Update logstash.sh

--- scripts/logstash.sh
+++ scripts/logstash.sh
@@ -2,7 +2,8 @@
 apt-get -y install logstash
 #
 /bin/systemctl daemon-reload
-systemctl enable logstash.service
+/bin/systemctl enable logstash.service
 #
 sed -i -e "s/User=logstash/User=root/g" /etc/systemd/system/logstash.service
 sed -i -e "s/Group=logstash/Group=root/g" /etc/systemd/system/logstash.service
+#
