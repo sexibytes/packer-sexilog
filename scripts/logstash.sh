94ebca7b076685decdafda1dc1522829565bfc56
Raphaël SCHITZ <raphael@schitz.net>
2022-06-28 23:40:06 +0200

    Update logstash.sh

--- scripts/logstash.sh
+++ scripts/logstash.sh
@@ -4,6 +4,6 @@ apt-get -y install logstash
 /bin/systemctl daemon-reload
 /bin/systemctl enable logstash.service
 #
-sed -i -e "s/User=logstash/User=root/g" /etc/systemd/system/logstash.service
-sed -i -e "s/Group=logstash/Group=root/g" /etc/systemd/system/logstash.service
+# sed -i -e "s/User=logstash/User=root/g" /etc/systemd/system/logstash.service
+# sed -i -e "s/Group=logstash/Group=root/g" /etc/systemd/system/logstash.service
 #
