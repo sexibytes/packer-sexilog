6d5264c7f97b367a90faa98269d87427a10aae5d
Raphaël SCHITZ <raphael@schitz.net>
2022-06-28 23:40:02 +0200

    Update elastic.sh

--- scripts/elastic.sh
+++ scripts/elastic.sh
@@ -3,14 +3,10 @@
 #
 apt-get install -y gnupg2
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 #
 apt-get update
 apt-get install -y elasticsearch
-#
-/bin/systemctl daemon-reload
-/bin/systemctl enable elasticsearch.service
-/bin/systemctl start elasticsearch.service
 #
 sed -i -e "s/#cluster\.name\: my-application/cluster\.name\: sexilog/g" /etc/elasticsearch/elasticsearch.yml
 sed -i -e "s/path\.data\: \/var\/lib\/elasticsearch/path\.data\: \/mnt\/efs\/elasticsearch/g" /etc/elasticsearch/elasticsearch.yml
@@ -19,16 +15,29 @@ sed -i -e "s/path\.data\: \/var\/lib\/elasticsearch/path\.data\: \/mnt\/efs\/ela
 # echo "index.number_of_replicas: 0" >> /etc/elasticsearch/elasticsearch.yml
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/index-templates.html
 #
-echo "bootstrap.memory_lock: true" >> /etc/elasticsearch/elasticsearch.yml
+sed -i -e "s/cluster.initial_master_nodes/#cluster.initial_master_nodes/g" /etc/elasticsearch/elasticsearch.yml
+#
 echo "indices.memory.index_buffer_size: 50%" >> /etc/elasticsearch/elasticsearch.yml
 #
 echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
+sed -i -e "s/#bootstrap.memory_lock/bootstrap.memory_lock/g" /etc/elasticsearch/elasticsearch.yml
+#
+# https://discuss.elastic.co/t/cannot-disable-security-in-8-1/299857
+sed -i -e "s/xpack\.security\.enabled\: true/xpack\.security\.enabled\: false/g" /etc/elasticsearch/elasticsearch.yml
+echo "xpack.security.transport.ssl.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
+echo "xpack.security.http.ssl.enabled: false" >> /etc/elasticsearch/elasticsearch.yml
 #
 chown -R elasticsearch:elasticsearch /mnt/efs/elasticsearch
 #
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service
+/bin/systemctl start elasticsearch.service
+#
+sleep 30
+#
 curl -X PUT "localhost:9200/_template/default" -H 'Content-Type: application/json' -d'
 { 
- "template" : "*",
+ "index_patterns": ["*"],
  "order": -1,
   "settings" : {
     "number_of_shards" : "1",
@@ -39,3 +48,5 @@ curl -X PUT "localhost:9200/_template/default" -H 'Content-Type: application/jso
 echo 'vm.swappiness = 1' >> /etc/sysctl.conf
 #
 # es.enforce.bootstrap.checks
+
+# apt-get install -y elasticsearch-curator
