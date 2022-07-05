7c3f113bb59697c50aff84c22da6c18c678aeac3
Raphaël SCHITZ <raphael@schitz.net>
2022-07-05 17:54:38 +0200

    Update kibana.sh

--- scripts/kibana.sh
+++ scripts/kibana.sh
@@ -6,11 +6,12 @@ apt-get -y install kibana
 sed -i -e 's/#server\.host: "localhost"/server\.host: "0\.0\.0\.0"/g' /etc/kibana/kibana.yml
 # disable telemetry
 echo "telemetry.enabled: false" >> /etc/kibana/kibana.yml
+echo "security.showInsecureClusterWarning: false" >> /etc/kibana/kibana.yml
 #
 /bin/systemctl daemon-reload
 /bin/systemctl enable kibana.service
 /bin/systemctl start kibana.service
-sleep 30s
+sleep 60s
 # Create index pattern
 curl -XPOST "http://localhost:5601/api/saved_objects/index-pattern/logstash-*" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d'
 {
@@ -39,6 +40,16 @@ curl -XPOST "http://localhost:5601/api/kibana/settings/accessibility:disableAnim
 curl -XPOST "http://localhost:5601/api/kibana/settings/securitySolution:enableNewsFeed" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d'
 {
   "value" : "false"
+}'
+#
+curl -XPOST "http://localhost:5601/api/kibana/settings/doc_table:hideTimeColumn" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d'
+{
+  "value" : "true"
+}'
+#
+curl -XPOST "http://localhost:5601/api/kibana/settings/defaultColumns" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d'
+{
+  "value" : ["message"]
 }'
 #
 # POST <kibana-host>/api/kibana/settings/theme:darkMode
