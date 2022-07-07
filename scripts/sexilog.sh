2ded210ecffd54af16987932482e11bae06737c3
Raphaël SCHITZ <raphael@schitz.net>
2022-07-07 12:15:33 +0200

    Update sexilog.sh

--- scripts/sexilog.sh
+++ scripts/sexilog.sh
@@ -6,3 +6,9 @@ cp -R /tmp/sexilog-dev/etc/logstash/conf.d/input-syslog-esxi.conf /etc/logstash/
 cp -R /tmp/sexilog-dev/etc/logstash/conf.d/filter-syslog-esxi.conf /etc/logstash/conf.d/
 cp -R /tmp/sexilog-dev/etc/logstash/conf.d/output-elasticsearch.conf /etc/logstash/conf.d/
 #
+cp -R /tmp/sexilog-dev/opt/elasticsearch-curator/curator.yml /opt/elasticsearch-curator/
+cp -R /tmp/sexilog-dev/opt/elasticsearch-curator/action.yml /opt/elasticsearch-curator/
+#
+cp -R /tmp/sexilog-dev/etc/logrotate.d/* /etc/logrotate.d/
+#
+# $(( $(df /mnt/efs | awk '/[0-9]%/{print $(NF-4)}') / 100 * 90 / 1048576))
