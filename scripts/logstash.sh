a3170e28212ce308766f585a7bea38c5598a7bd4
Raphaël SCHITZ <raphael@schitz.net>
2022-07-07 17:58:25 +0200

    Update logstash.sh

--- scripts/logstash.sh
+++ scripts/logstash.sh
@@ -5,8 +5,8 @@ apt-get -y install logstash
 /bin/systemctl enable logstash.service
 #
 # 514 port opening needs root
-sed -i -e "s/User=logstash/User=root/g" /usr/lib/systemd/system/logstash.service
-sed -i -e "s/Group=logstash/Group=root/g" /usr/lib/systemd/system/logstash.service
+sed -i -e "s/User=logstash/User=root/g" /etc/systemd/system/logstash.service
+sed -i -e "s/Group=logstash/Group=root/g" /etc/systemd/system/logstash.service
 # sed -i -e 's/LS_USER=\"logstash\"/LS_USER=\"root\"/g' /etc/default/logstash
 # sed -i -e 's/LS_GROUP=\"logstash\"/LS_GROUP=\"root\"/g' /etc/default/logstash
 #
