4fdbaee07dfcfaf810d419627aebb7b64053c283
Raphaël SCHITZ <raphael@schitz.net>
2022-07-07 18:52:39 +0200

    Update sexilog.sh

--- scripts/sexilog.sh
+++ scripts/sexilog.sh
@@ -12,3 +12,15 @@ cp -R /tmp/sexilog-dev/opt/elasticsearch-curator/action.yml /opt/elasticsearch-c
 cp -R /tmp/sexilog-dev/etc/logrotate.d/* /etc/logrotate.d/
 #
 # $(( $(df /mnt/efs | awk '/[0-9]%/{print $(NF-4)}') / 100 * 90 / 1048576))
+#
+echo "xpack.security.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+echo "xpack.security.transport.ssl.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+#
+/bin/systemctl restart elasticsearch.service
+#
+sleep 30
+#
+echo "Sex!L0g" | /usr/share/elasticsearch/bin/elasticsearch-keystore add -xf bootstrap.password
+#
+echo 'elasticsearch.username: "elastic"' >> /etc/kibana/kibana.yml
+echo 'elasticsearch.password: "Sex!L0g"' >> /etc/kibana/kibana.yml
